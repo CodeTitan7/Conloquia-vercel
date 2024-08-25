67c8436107d44ab247f9ee8acab579ef8f2afda8
VDevanarayan <octavius1983bc@gmail.com>
2024-08-25 19:51:01 +0530

    updated build.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-pip install setuptools
-pip install -r requirements.txt
+# Update pip and setuptools
+pip install --upgrade pip setuptools
 
+# Install dependencies from requirements.txt
+pip install --no-cache-dir -r requirements.txt
+
+# Run Django migrations
 python manage.py makemigrations
 python manage.py migrate
+
+# Collect static files (if applicable)
+python manage.py collectstatic --noinput
